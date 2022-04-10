8d939b49829796297660b00d2aac81f62779da60
okita-haruru <okitaharuru@gmail.com>
2022-04-10 17:38:15 +0800

    add models

---
+++ contracts/Model.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.0;
+
+contract Model {
+    ProductionUnit[] productionUnits;
+    
+    struct ProductionUnit {
+        //address Addr;
+        uint32 ID;
+        bool banned;
+        uint8 score;
+        string name;
+        address[] subAddr;
+    }
+    struct User {
+        //bytes32 ID;
+        bytes32 password;
+        bool banned;
+    }
+    // struct Product{
+    //     bytes32 ID;
+    //     string name;
+    // }
+    // struct Admin{
+    //     address Addr;
+    //     string name;
+    // }
+}
