e39ed71396ffa1bda5d2d6b02c4a144d18dc09b6
okita-haruru <okitaharuru@gmail.com>
2022-04-13 20:28:57 +0800

    delete description

--- contracts/ProductBase.sol
+++ contracts/ProductBase.sol
@@ -6,24 +6,10 @@ interface ProductBase {
         uint32 _productionUnitID,
         uint16 _num
     ) external returns (bytes32[] memory IDs);
-    // {
-    //     //IDs = new uint[](_num);
-    //     for (uint16 i = 0; i < _num; i++) {
-    //         IDs[i] = keccak256(
-    //             abi.encodePacked(_productionUnitID, _timeStamp, i)
-    //         );
-    //         emit Confirm(IDs[i], _productionUnitID, 0, _timeStamp);
-    //     }
-    // }
 
     function confirm(
         bytes32 _productionID,
         uint32 _productionUnitID,
         uint8 _state
     ) external;
-    // {
-    //     emit Confirm(_productionID, _productionUnitID, _state, _timeStamp);
-    // }
-
-
 }
