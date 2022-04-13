c9ba3c659315b7d0f52d6751d2a55fe1c7ef8a0a
okita-haruru <okitaharuru@gmail.com>
2022-04-13 21:29:37 +0800

    modify functions

--- contracts/ComplainQueue.sol
+++ contracts/ComplainQueue.sol
@@ -1,29 +1,39 @@
 pragma solidity ^0.8.0;
 import "./Model.sol";
-contract ComplainQueue is Model
-{
+
+contract ComplainQueue is Model {
     struct ComplainQueue {
-        Comlaint[] data;
-        uint front;
-        uint rear;
+        Complain[] data;
+        uint256 front;
+        uint256 rear;
     }
- 
-    
+
     // push
-    function push(ComplainQueue storage q, Comlaint storage data) internal
-    {
-        if ((q.rear + 1) % q.data.length == q.front)
-           pop(q); // throw first;
+    function push(ComplainQueue storage q, Complain memory data) internal {
+        if ((q.rear + 1) % q.data.length == q.front) pop(q); // throw first;
         q.data[q.rear] = data;
         q.rear = (q.rear + 1) % q.data.length;
     }
+
     // pop
-    function pop(ComplainQueue storage q) internal returns (Comlaint storage dat)
+    function pop(ComplainQueue storage q)
+        internal
+        returns (Complain storage dat)
     {
-        require(q.rear!=q.front);
-        Comlaint storage r = q.data[q.front];
-        dat=r;
+        require(q.rear != q.front);
+        Complain storage r = q.data[q.front];
+        dat = r;
         delete q.data[q.front];
         q.front = (q.front + 1) % q.data.length;
+    }
+
+    function getFirst(ComplainQueue storage q)
+        internal
+        view
+        returns (Complain storage dat)
+    {
+        require(q.rear != q.front);
+        Complain storage r = q.data[q.front];
+        dat = r;
     }
 }
